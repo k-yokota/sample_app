82badad529ac6bf8bbbf948c5464c6722579137b
kunioki <ceh.neg.miin.calneer@gmail.com>
2015-06-15 11:53:38 +0900

    Add a full_title helper

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,19 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-require 'securerandom'
-
-def secure_token
-  token_file = Rails.root.join('.secret')
-  if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
-  else
-    # Generate a new token and store it in token_file.
-    token = SecureRandom.hex(64)
-    File.write(token_file, token)
-    token
-  end
-end
-
-SampleApp::Application.config.secret_key_base = secure_token
+SampleApp::Application.config.secret_key_base = '6a2d18355de6885655825621e49830d10fa74dae849a92cfabcad98bf2f797108e608920939bee5076745ef570d0b24a2a959a3bab7ab969fea388b9ff3bba47'
